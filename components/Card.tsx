7da94d082d52c9c8d3dc0cc6bf0332d4beb69d5d
George Campbell <campbell.goe@gmail.com>
2023-03-11 11:14:23 +0000

    add test for Card component

--- components/Card.tsx
+++ components/Card.tsx
@@ -2,7 +2,7 @@ import styled from "styled-components"
 import Image from 'next/image'
 
 const Card = styled(({ className = '', image, code }) => {
-  return <div className={className + ' Card'}>
+  return <div className={className + ' Card'} data-testid="card">
     {image && <Image src={image} alt={code} width={226} height={314}/>}
   </div>
 })`
--- tests/app.test.js
+++ tests/app.test.js
@@ -2,6 +2,7 @@ import '@testing-library/jest-dom'
 import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import PickCard from '../components/PickCard';
+import Card from '../components/Card';
 
 describe("PickCard component", () => {
   it("should render a button with text \"Draw card\"", () => {
@@ -13,5 +14,33 @@ describe("PickCard component", () => {
     const button = screen.getByText("Draw card")
     expect(button).toBeInTheDocument()
   
+  })
+})
+
+describe("Card component", () => {
+  it("should render a bordered space for a card if no image provided", () => {
+    render(<Card className="TestCard" code="TestCard0" />)
+    const card = screen.getByTestId('card')
+    expect(card).toBeInTheDocument()
+
+    expect(
+      window
+        .getComputedStyle(card)
+        .getPropertyValue("border")
+    ).toEqual('1px solid black');
+
+
+    const image = card.querySelector("img")
+    expect(image).not.toBeInTheDocument()
+
+    
+  })
+  // TODO: this needs updating if using <picture> and <source> instead of <img>
+  it("should render an image", () => {
+    const imageSrc = 'data:image/gif;base64,R0lGODlhAQABAAAAACw='
+    render(<Card className="TestCard" code="TestCard1" image={imageSrc}/>)
+    const card = screen.getByTestId('card')
+    const image =  card.querySelector("img")
+    expect(image).toBeInTheDocument()
   })
 })
